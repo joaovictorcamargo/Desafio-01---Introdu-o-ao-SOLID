db70144e2c8189359e62b23b9f48f1bcde2842ad
joaovictor <jc7155004@gmail.com>
2022-05-29 14:09:21 -0300

    test pass

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import SwaggerUi from "swagger-ui-express";
 
+import swaggerFile from "./swagger.json";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
